90183891abd8440db0f9dabeba8f192a165f1a75
Chetan Patil <chetanpatil323212@gmail.com>
2026-08-31 19:42:40 +0530

    delete buttton function complet

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-
 let addbtn = document.querySelector("#addBtn");
 let takslist = document.querySelector("#taskList");
 let taskInput = document.querySelector("#taskInput");
 let input = taskInput.value;
 
+function addButton() {
+  addbtn.addEventListener("click", function () {
+    let task = document.createElement("li");
 
-function addButton(){
-    addbtn.addEventListener("click", function(){
-        let task = document.createElement("li")
-        task.textContent = taskInput.value;
-        takslist.appendChild(task)
-    })
+    if (taskInput.value == "") {
+      alert("Enter your task");
+    }else{
+    // delete button create
+    let Deletebtn = document.createElement("button");
+    Deletebtn.textContent = "Complet";
+    Deletebtn.setAttribute("id", "delebtn");
+    task.textContent = taskInput.value;
+    takslist.appendChild(task);
+    task.appendChild(Deletebtn);
+    
+
+    // delete button event
+    Deletebtn.addEventListener("click", function () {
+      task.remove()
+    });
+   } 
+  });
 }
 
-addButton()
+function deleteButton() {}
+
+addButton();
